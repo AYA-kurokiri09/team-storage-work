1a154e64bcc3fd9eb8b10fad05e97c22a92df0e9
水谷綾乃 <aya@mizutaniayanonoMacBook-Pro.local>
2020-08-02 22:48:03 +0900

    build docker and fit laravel, sass

---
+++ resources/views/service/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/service.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
+    <title>Service</title>
+</head>
+<body>
+    <header>
+    <a href="#">ログイン</a>
+    <a href="#">新規登録</a>
+    <i class = "fas fa-bars"></i>
+    </header>
+    <div>
+
+    </div>
+</body>
+</html>
