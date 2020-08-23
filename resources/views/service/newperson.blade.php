36cbfafc8eee645bb7ee5b0bc0c75331249c0227
水谷綾乃 <ice6e63@gmail.com>
2020-08-24 07:58:18 +0900

    complete css newperson.blade.php

--- resources/views/service/newperson.blade.php
+++ resources/views/service/newperson.blade.php
@@ -18,23 +18,22 @@
     <hr>
     <form action="{{route('service.personAdd')}}" method="post">
     @csrf
-    <label for="name">名前</label>&nbsp;<input type="text" name="name" id="inputArea"><br>
+    <label for="name" id="name">名前</label>&nbsp;<input type="text" name="name" id="inputArea"><br>
     <label for="email">メールアドレス</label>&nbsp;<input type="text" name="email" id="inputArea"><br>
-    <label for="password">パスワード</label>&nbsp;<input type="text" name="password" id="inputArea"><br>
+    <label for="password" id="password">パスワード</label>&nbsp;<input type="text" name="password" id="inputArea"><br>
     <label for="password">所属部署　※1つ選択して下さい</label><br>
-    <input type="radio" name="department" value="営業">営業部<br>
-    <input type="radio" name="department" value="製造">製造部<br>
-    <input type="radio" name="department" value="研究開発">研究開発部<br>
-    <input type="radio" name="department" value="運送">運送部<br>
-    <input type="radio" name="department" value="総務">総務部<br>
+    <input type="radio" name="department" id="department" value="営業">営業部<br>
+    <input type="radio" name="department" id="department" value="製造">製造部<br>
+    <input type="radio" name="department" id="department" value="研究開発">研究開発部<br>
+    <input type="radio" name="department" id="department" value="運送">運送部<br>
+    <input type="radio" name="department" id="department" value="総務">総務部<br>
     <br>
-    <input type="radio" name="role" value="manager">課長職以上の方はチェックして下さい
+    <input type="radio" name="role" value="manager" id="manager">課長職以上の方はここにチェックを入れて下さい
     <input type="radio" name="role" value="employee" checked="checked" id="radio_emp">
     <!-- radio_emp側をcheckedとしているので、見えているラジオボタンにチェックを入れないとrole = employeeとなる -->
     <br>
     <input type="submit" id="register" value="登録">
     </form>
-    <p>登録後、ログインするとサービスが使えます！</p>
     <a href="{{route('service.index')}}" id="login">
     <h4>登録をキャンセルして</h4>戻る</a>
     </div> 
