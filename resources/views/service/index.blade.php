8a55e77e50f5573fd412a1d7fe8858e2b674f6c9
水谷綾乃 <ice6e63@gmail.com>
2020-08-29 07:04:13 +0900

    index.blade.php 但し書き追加

--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link rel="stylesheet" href="{{ mix('css/service.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/service.css') }}"> <!-- ローカルで動かない場合はmix->assetとする -->
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
     <title>クラウドストレージ　虎の巻</title>
 </head>
