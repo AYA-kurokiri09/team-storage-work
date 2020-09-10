9bffc57b08f9bfa2d4774e22b5a01197bef26c9a
AYA-kurokiri09 <ice6e63@gmail.com>
2020-09-11 06:48:12 +0900

    新規登録時のバリデーシ ョン実装 デプロイ

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/service');/*元はreturn view('welcome')だった*/
+    return redirect('/service');/*元はreturn view('welcome')だった*/ 
 });
 
 Auth::routes();
