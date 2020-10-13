2fb3248657ad86b60154e077193a42bb8c332ac4
AYA <ice6e63@gmail.com>
2020-10-14 01:29:18 +0900

    扉ページ編集　intro.blade.php

--- resources/views/service/intro.blade.php
+++ resources/views/service/intro.blade.php
@@ -4,34 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link rel="stylesheet" href="{{ mix('css/service.css') }}"> <!-- ローカルで動かない場合はmix->assetとする -->
+    <link rel="stylesheet" href="{{ asset('css/service_intro.css') }}"> <!-- ローカルで動かない場合はmix->assetとする -->
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <title>クラウドストレージ　虎の巻</title>
 </head>
-<!--デザインした通りにコーディングする-->
 <body>
-    <header>
-    <i class = "fas fa-bars"></i>
+<header>
+    <div>
+    <h2>虎の巻　</h2><h4>シンプルなチーム用クラウドストレージ</h4>
+    </div>
+    <div>
+    <a href="">利用開始</a>
+    <a href="">説明を見る</a>
+    </div>
     </header>
-    <img src="{{asset('img/tiger_left.jpg')}}" alt="">
-    <hr noshade>
     <div class="login_container">
     <h3>クラウドストレージ</h3>
     <h1>虎の巻</h1>
-    <p>{!! nl2br(e($message)) !!}</p>
-    <form action="/service" method="post">
-    @csrf
-    <label for="name" id="name">名前</label>&nbsp;<input type="text" name="name" id="inputArea"><br>
-    <label for="password">パスワード</label>&nbsp;<input type="password" name="password" id="inputArea"><br>
-    <input type="submit" id="login" value="ログイン">
-    </form>
+    
     <a href="{{route('service.newperson')}}", id="register">
     <h4>初めての方はこちら</h4>新規登録</a>
     </div>
     
     
 
-                
+<script src="{{ asset('js/service_intro.js') }}"></script>                
 </body>
 </html>
 
